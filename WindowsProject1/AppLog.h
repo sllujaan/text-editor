9a8e66f450aa64b632ec22ecc416dec42760e063
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-23 12:55:22 +0500

    appDebuging bugs fixes

--- WindowsProject1/AppLog.h
+++ WindowsProject1/AppLog.h
@@ -3,6 +3,7 @@
 #include<string>
 using namespace std;
 
+#ifdef WIN32_DEBUG
 
 #ifndef LOG_WCHAR 
 #define LOG_WCHAR(x) OutputDebugStringW((LPCWSTR)x); \
@@ -10,33 +11,31 @@ using namespace std;
 #endif // !LOG(x)
 
 #ifndef LOG_INT 
-#define LOG_INT(x) \
-do { \
+#define LOG_INT(x) {\
 	wstring ws = to_wstring(x); \
 	OutputDebugStringW((LPCWSTR)ws.c_str()); \
-	OutputDebugStringW((LPCWSTR)L"\r\n"); /*for new line*/ \
-} \
-while (0);
+	OutputDebugStringW((LPCWSTR)L"\r\n");}
 #endif // !LOG_INT 
 
 #ifndef LOG_STR
-#define LOG_STR(x) \
-do { \
+#define LOG_STR(x) {\
 	wstring ws = wstring(x.begin(), x.end()); \
 	OutputDebugStringW((LPCWSTR)ws.c_str()); \
-	OutputDebugStringW((LPCWSTR)L"\r\n"); /*for new line*/ \
-} \
-while (0);
+	OutputDebugStringW((LPCWSTR)L"\r\n");}
 #endif // !LOG_STR
 
 #ifndef LOG_CHAR
-#define LOG_CHAR(x) \
-do { \
+#define LOG_CHAR(x) {\
 	string s(1, x);\
-	LOG_STR(s);\
-} \
-while (0);
+	LOG_STR(s);}
 #endif // !LOG_CHAR
+
+#else
+#define LOG_WCHAR
+#define LOG_INT
+#define LOG_STR
+#define LOG_CHAR
+#endif // WIN32_DEBUG
 
 
 void LOG_LAST_ERROR();
